b3d2197290b5ce9e9ae2703409394eff7ca1bc0c
FokhrulSabel <fokhrulislam316@gmail.com>
2026-01-30 20:25:30 +0600

    save profile pic to imgbb and update user profile error fix

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
         formData.append("image", profileImg);
 
         // 2. send the photo to store and get the ul
-        const image_API_URL = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host_key}`;
+        const image_API_URL = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host}`;
 
         axios.post(image_API_URL, formData).then((res) => {
           console.log("after image upload", res.data.data.url);
