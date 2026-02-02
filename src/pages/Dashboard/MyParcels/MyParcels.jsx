07b99cff74ba53c0496cdce4d80b8df7a8e05c1b
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-02 21:48:52 +0600

    Payment method in myParcels page

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -50,6 +50,20 @@ const MyParcels = () => {
     });
   };
 
+  //payment handel
+  const handlePayment = async (parcel) => {
+        const paymentInfo = {
+            cost: parcel.cost,
+            parcelId: parcel._id, 
+            senderEmail: parcel.senderEmail,
+            parcelName: parcel.parcelName
+        }
+        const res = await axiosSecure.post('/payment-checkout-session', paymentInfo);
+
+        console.log(res.data.url);
+        window.location.href = res.data.url;
+    }
+
   return (
     <div>
       <h2>All of my parcels : {parcels.length}</h2>
@@ -75,13 +89,17 @@ const MyParcels = () => {
                 <td>
                   {parcel.paymentStatus === "paid" ? (
                     <span className="text-green-400">Paid</span>
-                  ) : (
-                    <Link to={`/dashboard/payment/${parcel._id}`}>
-                      <button className="btn btn-sm btn-primary text-black">
-                        Pay
-                      </button>
-                    </Link>
-                  )}
+                  ) : 
+                  //   <Link to={`/dashboard/payment/${parcel._id}`}>
+                  //     <button className="btn btn-sm btn-primary text-black">
+                  //       Pay
+                  //     </button>
+                  //   </Link>
+                  // 
+
+                  <button onClick={() => handlePayment(parcel)} className="btn btn-sm btn-primary text-black">Pay</button>
+                  
+                  }
                 </td>
                 <td>{parcel.deliveryStatus}</td>
                 <td>
