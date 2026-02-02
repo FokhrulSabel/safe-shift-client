36581c57fe95ffca2b66edf0db62a68aeeca2adc
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-02 23:51:32 +0600

    display transaction id and tracking id

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useSearchParams } from "react-router";
 
 const PaymentSuccess = () => {
   const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
   const sessionId = searchParams.get("session_id");
   const axiosSecure = useAxiosSecure();
 
@@ -15,12 +16,18 @@ const PaymentSuccess = () => {
         .patch(`/payment-success?session_id=${sessionId}`)
         .then((res) => {
           console.log(res.data);
+          setPaymentInfo({
+            transactionId: res.data.transactionId,
+            trackingId: res.data.trackingId,
+          });
         });
     }
   }, [sessionId, axiosSecure]);
   return (
     <div>
       <h2 className="text-4xl">Payment successful</h2>
+      <p>Your TransactionId: {paymentInfo.transactionId}</p>
+      <p>Your Parcel Tracking id: {paymentInfo.trackingId}</p>
     </div>
   );
 };
