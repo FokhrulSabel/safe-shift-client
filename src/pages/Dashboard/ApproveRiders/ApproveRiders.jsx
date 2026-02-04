bf6490c70611c856f673ed593495031d473a5011
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-05 00:38:27 +0600

    show parcels pending pickup and open dialogue to assign riders

--- src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
+++ src/pages/Dashboard/ApproveRiders/ApproveRiders.jsx
@@ -52,7 +52,8 @@ const ApproveRiders = () => {
               <th>Name</th>
               <th>Email</th>
               <th>District</th>
-              <th>status</th>
+              <th>Application Status</th>
+              <th>Work Status</th>
               <th>Actions</th>
             </tr>
           </thead>
@@ -70,6 +71,7 @@ const ApproveRiders = () => {
                     {rider.status}
                   </p>
                 </td>
+                <td>{rider.workStatus}</td>
                 <td>
                   <button className="btn">
                     <FaEye></FaEye>
